503322a0886a376226137c4191e81611514d1e5b
BrunoUemura <bruno.h.uemura@outlook.com>
2021-12-06 02:04:10 -0300

    Add environment variable

--- src/main/java/com/hide/weather/service/WeatherService.java
+++ src/main/java/com/hide/weather/service/WeatherService.java
@@ -2,16 +2,22 @@ package com.hide.weather.service;
 
 import com.hide.weather.model.Weather;
 import com.hide.weather.model.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
 
+    @Value("${api.url}")
+    private String externalAPIUrl;
+
     public WeatherResponse findByCity(String city) {
-        String externalAPIUrl = "http://api.url";
+        String apiUrl = this.externalAPIUrl + city;
         RestTemplate restTemplate = new RestTemplate();
-        Weather weather = restTemplate.getForObject(externalAPIUrl, Weather.class);
+        Weather weather = restTemplate.getForObject(apiUrl, Weather.class);
 
         WeatherResponse weatherResponse = new WeatherResponse(
                 weather.getLocation().getName(),
